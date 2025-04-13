79eae0f8d6be917eb5a26da99c54335da017f451
likanug <likanug.g@qq.com>
2025-04-13 23:12:53 +0800

    移除对syscall的引用，简化信号处理逻辑

--- chapter1/web/web_server.go
+++ chapter1/web/web_server.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"syscall"
 	"time"
 )
 
@@ -32,7 +31,6 @@ func main() {
 	}
 
 	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
 	signal.Notify(ch, os.Interrupt, os.Kill)
 
 	go func() {
