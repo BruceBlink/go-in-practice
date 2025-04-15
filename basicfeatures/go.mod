f7dcac922c5801c42ada752a82673a9a32fe36b9
likanug <likanug.g@qq.com>
2025-04-15 21:05:56 +0800

    指针的基本运用

---
+++ basicfeatures/go.mod
@@ -0,0 +1,3 @@
+module basicfetures
+
+go 1.24.1
---
+++ basicfeatures/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+//"math/rand"
+
+func main() {
+	//fmt.Println("Value:", rand.Int())
+	//const price float32 = 275.00
+	//const tax float32 = 27.50
+	//fmt.Println(price + tax)
+	/**
+	first := 100
+	second := &first
+	first ++
+	fmt.Println("first: ", first)  //   101
+	fmt.Println("second: ", *second) // 101
+	**/
+	first := 100
+	second := &first
+
+	first++
+	*second++
+
+	fmt.Println("first: ", first)  //   102
+	fmt.Println("second: ", *second) // 102
+	var myNewPoint = second
+	*myNewPoint ++
+
+	fmt.Println("first: ", first)  //   103
+	fmt.Println("second: ", *second) // 103
+
+}
