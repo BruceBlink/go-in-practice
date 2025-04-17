028e83fd0499ed0ee7376dcda1ea321f7dbb1df1
likanug <likanug.g@qq.com>
2025-04-17 20:10:28 +0800

    在operations中添加goroutine

--- concurrency/operations.go
+++ concurrency/operations.go
@@ -5,7 +5,8 @@ import "fmt"
 func CalcStoreTotal(data ProductData) {
 	var storeTotal float64
 	for category, group := range data {
-		storeTotal += group.TotalPrice(category)
+		//storeTotal += group.TotalPrice(category)
+		go group.TotalPrice(category)
 	}
 	fmt.Println("Total:", ToCurrency(storeTotal))
 }
